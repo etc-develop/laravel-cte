6c7fb34b4d95c9963f6a35e4c4359ad844f72448
Ryan <a05151988@gmail.com>
2026-08-04 08:53:45 +0800

    重構 OracleGrammar 類別，新增 prepareBindingsForUpdate 方法以處理更新語句的綁定

--- src/Query/Grammars/OracleGrammar.php
+++ src/Query/Grammars/OracleGrammar.php
@@ -7,5 +7,29 @@ use Yajra\Oci8\Query\Grammars\OracleGrammar as Base;
 
 class OracleGrammar extends Base implements ExpressionGrammar
 {
-    use CompilesOracleExpressions;
+    use CompilesOracleExpressions {
+        // 將 Trait 裡面原有的 prepareBindingsForUpdate 重命名，避免直接衝突
+        prepareBindingsForUpdate as traitPrepareBindingsForUpdate;
+    }
+
+    /**
+     * Prepare the bindings for an update statement.
+     *
+     * @param array{expressions: list<mixed>, select: list<mixed>, from: list<mixed>, join: list<mixed>,
+     *     where: list<mixed>, having: list<mixed>, order: list<mixed>, union: list<mixed>,
+     *     unionOrder: list<mixed>} $bindings
+     * @param array<string, mixed> $values
+     * @return array<int, mixed>
+     */
+    public function prepareBindingsForUpdate(array $bindings, array $values): array
+    {
+        $values = array_merge($bindings['expressions'], $values);
+
+        unset($bindings['expressions']);
+
+        /** @var array<int, mixed> $bindings */
+        $bindings = parent::prepareBindingsForUpdate($bindings, $values);
+
+        return $bindings;
+    }
 }
